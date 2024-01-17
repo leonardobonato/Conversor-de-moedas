8e86ddb16851f44de3705f550b742625f1418b36
Leonardo Bonato <123083304+leonardobonato@users.noreply.github.com>
2024-01-17 09:42:10 -0300

    atualização valores em tempo real consumindo api

--- codigo.js
+++ codigo.js
@@ -1,14 +1,17 @@
 const button = document.getElementById('convert-button') //pra pegar o meu botão la no html e trazer pra ca
 const select = document.getElementById('currency-select') //pra pegar meu select la no html e trazer pra ca
 
-const dolar = 5.2
-const euro = 5.5
-const coin = 141.445
-
-const convertValues = () => {
+const convertValues = async () => {
     const inputReais = document.getElementById('input-real').value //.value é pra pegar somente o valor do meu input, só o que foi digitado
     const realValueText = document.getElementById('real-value-text')
     const currencyValueText = document.getElementById('currency-value-text')
+
+    const dados = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then( response => response.json())
+
+    console.log(dados)
+    const dolar = dados.USDBRL.high
+    const euro = dados.EURBRL.high
+    const btc = dados.BTCBRL.high
 
     realValueText.innerHTML = new Intl.NumberFormat('pt-BR', //Pra formatar meu valor pra moeda real
         { style: 'currency', currency: 'BRL' }
@@ -33,7 +36,7 @@ const convertValues = () => {
     if(select.value ==='BitCoin'){
         currencyValueText.innerHTML = new Intl.NumberFormat('BTC', //Pra formatar meu valor pra moeda bitcoin
         { style: 'currency', currency: 'BTC' }
-    ).format(inputReais / coin);
+    ).format(inputReais / btc);
 
     }
 
